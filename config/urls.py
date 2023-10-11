4844067095b1bfb8fd00553f753ea4fac60cf96e
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-10-11 09:36:52 -0300

    tutorial rest 03

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 # config/urls.py
 
 # config/urls.py
+# config/urls.py
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
